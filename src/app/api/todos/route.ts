e0afa0fc77e43baf77ed092b6ef00f3bc1187c8b
LizandroBackend <codeversebackend@gmail.com>
2024-07-12 14:24:17 -0600

    ci: Add Skip and Take to Endpoint Todos | Dev: Lizandro

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,8 +1,21 @@
 import { NextResponse, NextRequest } from "next/server";  
 import prisma from "@/app/lib/prisma";
  
-export async function GET(request: Request) {  
-    const todos = await prisma.todo.findMany(); 
+export async function GET(request: Request) {   
+    const { searchParams } = new URL(request.url) 
+    const take = Number(searchParams.get('take') ?? '10');  
+    const skip = Number(searchParams.get('skip') ?? '0'); 
+     
+    if (isNaN(skip)) { 
+        return NextResponse.json({ message: 'Skip tiene que ser un número'});
+    }
+     
+    
+    if ( isNaN(take)) { 
+        return NextResponse.json({ message: 'Take tiene que ser un número'}, { status: 400});
+    }
+
+    const todos = await prisma.todo.findMany({ take, skip, }); 
      
     return NextResponse.json(todos);
 }
