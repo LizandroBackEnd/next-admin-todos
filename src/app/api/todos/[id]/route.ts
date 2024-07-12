e8e49592cc748fd8848002d5de694f897368fafe
LizandroBackend <codeversebackend@gmail.com>
2024-07-12 14:59:18 -0600

    ci: Add new funcion endpoint for id and params | Dev: Lizandro

---
+++ src/app/api/todos/[id]/route.ts
@@ -0,0 +1,21 @@
+import prisma from "@/app/lib/prisma";
+import { stat } from "fs";
+import { NextResponse, NextRequest } from "next/server"; 
+   
+interface Segments { 
+    params: {
+        id: string;
+    }
+} 
+
+export async function GET(request: Request, {params}: Segments) { 
+     
+    const { id } = params; 
+    const todo = await prisma.todo.findFirst({where: { id } }); 
+     
+    if (!todo ) {
+        return NextResponse.json({ message: `Todo con id ${id} no existe` }, { status: 404 });
+    } 
+     
+    return NextResponse.json(todo);
+}
